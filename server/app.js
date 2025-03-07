26d340ff16b2ce40d8d73a400af9e9eee079e9db
AbdulazizRus <coderdim@gmail.com>
2025-03-07 20:50:32 +0500

    backend integration auth

--- server/app.js
+++ server/app.js
@@ -3,17 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 const bootstrap = async () => {
   try {
     const PORT = process.env.PORT || 6000;
-    morgan
+    mongoose
+      .connect(process.env.DATABASE_URL)
+      .then(() => console.log('MongoDB connected'));
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   } catch (error) {
     console.error(error);
   }
 };
+
+bootstrap();
