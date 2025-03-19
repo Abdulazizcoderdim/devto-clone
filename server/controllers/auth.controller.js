a353212c65e6d38c95a8057cf1e84cdb0859cd4a
AbdulazizRus <coderdim@gmail.com>
2025-03-19 14:44:53 +0500

    auth controller added

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,9 +5,9 @@ const prisma = new PrismaClient();
 class AuthController {
   async login(req, res, next) {
     try {
-      const { email, password } = req.body;
+      const { email } = req.body;
 
-      if (!email || !password) {
+      if (!email) {
         return next(BaseError.BadRequest('Email and password are required'));
       }
 
@@ -21,8 +21,38 @@ class AuthController {
         await mailService.sendOtp(existUser.email);
         return res.status(200).json({ email: existUser.email });
       }
+
+      const newUser = await prisma.user.create({
+        data: {
+          email,
+        },
+      });
+      await mailService.sendOtp(newUser.email);
+      return res.status(200).json({ email: newUser.email });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async verify(req, res, next) {
+    try {
+      const { email, otp } = req.body;
+      const result = await mailService.verifyOtp(email, otp);
+      if (result) {
+        const user = await prisma.user.update({
+          where: {
+            email,
+          },
+          data: {
+            isVerified: true,
+          },
+        });
+        return res.status(200).json({ user });
+      }
     } catch (error) {
       next(error);
     }
   }
 }
+
+module.exports = new AuthController();
