e227998d8163069dd3301ca99b343e3120b25bc3
AbdulazizRus <coderdim@gmail.com>
2025-03-23 08:56:52 +0500

    token service

--- server/services/token.service.js
+++ server/services/token.service.js
@@ -1,11 +1,64 @@
-const jwt = require('jsonwebtoken')
-
+const jwt = require("jsonwebtoken");
+const prisma = require("../config/prismaClient");
 
 class TokenService {
-    generateToken(payload){
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_KEY, {expiresIn: '15m'})
-        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_KEY,  { expiresIn: '30d'})
+  generateToken(payload) {
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_KEY, {
+      expiresIn: "15m",
+    });
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_KEY, {
+      expiresIn: "30d",
+    });
 
-        return {accessToken, refreshToken}
+    return { accessToken, refreshToken };
+  }
+
+  async saveToken(userId, refreshToken) {
+    const existToken = await prisma.token.findFirst({
+      where: {
+        userId,
+      },
+    });
+
+    if (existToken) {
+      return await prisma.token.update({
+        where: { userId },
+        data: { refreshToken },
+      });
     }
+
+    return await prisma.token.create({
+      data: { userId, refreshToken },
+    });
+  }
+
+  async removeToken(refreshToken) {
+    return await prisma.token.delete({
+      where: { refreshToken },
+    });
+  }
+
+  async findToken(refreshToken) {
+    return await prisma.token.findFirst({
+      where: { refreshToken },
+    });
+  }
+
+  validateRefreshToken(token) {
+    try {
+      return jwt.verify(token, process.env.JWT_REFRESH_KEY);
+    } catch (error) {
+      return null;
+    }
+  }
+
+  validateAccessToken(token) {
+    try {
+      return jwt.verify(token, process.env.JWT_ACCESS_KEY);
+    } catch (error) {
+      return null;
+    }
+  }
 }
+
+module.exports = new TokenService();
